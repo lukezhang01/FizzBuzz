b356e64dc4af3e0eca11c2f3b1bd39dbc8f75ce4
lukez999 <zluke7111@gmail.com>
2023-09-11 14:22:58 -0400

    added Main.java

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] strArray = new String[0];
+        FizzBuzz.main(strArray);
+        FizzBuzz.doFizzBuzz(5);
+    }
+}
